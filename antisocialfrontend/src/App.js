bb5fad74790fc8911db8d61c5f241a9118877e6c
Karen k  sin <karenksin@MacBook-Pro.local>
2020-01-18 15:56:38 -0800

    setup

---
+++ antisocialfrontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+  
+        <p>
+         test 
+        </p>
+          
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ antisocialfrontend/src/components/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
+
+
+
+export default function () {
+    return (
+        <div>
+            
+        </div>
+    )
+}
