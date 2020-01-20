9f1a7358489ca15f81cd56e925fd5a99769e5d40
sugoh <gsufen@gmail.com>
2020-01-20 04:57:27 -0800

    Removed unused sections and divs

--- antisocialfrontend/src/components/Header.js
+++ antisocialfrontend/src/components/Header.js
@@ -11,25 +11,27 @@ class Header extends Component {
                 <header>
                     <Navlink />
                     <main role="main">
-                        <div className="container h-100">
-                            <div className="row h-100 align-items-center">
-                                <div className="col">
+                        <div className="hero container h-100 align-items-center">
+                                <section className="col col-text">
+                                    <h1 className="col-12 text-left display-linebreak">
+                                        Better friendships,
+                                    </h1>
+                                    <h1 className="col-12 text-left display-linebreak">
+                                        stress-free.
+                                    </h1>
                                     <section>
-                                        <h1 className='col-12 text-left  display-linebreak'>
-                                            Better friendships, stress-free.
-                                        </h1>
+                                        <h3 className="text-left display-linebreak subtitle">
+                                            Superpotato is a personal assistant that helps you remember, manage, and grow your social network.
+                                        </h3>
+                                        <h3 className="text-left display-linebreak subtitle">
+                                            Stop messing with spreadsheets and chillax like a real potato.
+                                        </h3>
                                     </section>
-                                    <section>
-                                        <h3 className="col-12 text-left display-linebreak" > Superpotato is a personal assistant that helps you remember, manage, and grow your social network.
-                                        Stop messing with spreadsheets and chillax like a real potato.
-                                         </h3>
-                                        <CTA />
-                                    </section>
-                                </div>
-                                <div className="col">
+                                    <CTA />
+                                </section>
+                                <section className="col col-img">
                                     <img className="IphoneXMock" alt="profile" src={process.env.PUBLIC_URL + "/img/landing_mock.png"} />
-                                </div>
-                            </div>
+                                </section>
                         </div>
 
                     </main>
