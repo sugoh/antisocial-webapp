10d33f4d53b02ce0725224b2a4311c73eb6094b4
sugoh <gsufen@gmail.com>
2020-01-22 02:08:12 -0800

    Fixed formatting bugs. Need to add appropriate @media queries

--- antisocialfrontend/src/components/Header.js
+++ antisocialfrontend/src/components/Header.js
@@ -1,43 +1,36 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navlink from './Nav.js';
-import CTA from './CTA.js';
 
 class Header extends Component {
 
     render() {
         return (
-            <div>
-                <header>
-                    <Navlink />
-                    <main role="main">
-                        <div className="hero container h-100 align-items-center">
-                                <section className="col col-text">
-                                    <h1 className="col-12 text-left display-linebreak">
-                                        Better friendships,
-                                    </h1>
-                                    <h1 className="col-12 text-left display-linebreak">
-                                        stress-free.
-                                    </h1>
-                                    <section>
-                                        <h3 className="text-left display-linebreak subtitle">
-                                            Superpotato is a personal assistant that helps you remember, manage, and grow your social network.
-                                        </h3>
-                                        <h3 className="text-left display-linebreak subtitle">
-                                            Stop messing with spreadsheets and chillax like a real potato.
-                                        </h3>
-                                    </section>
-                                    <CTA />
-                                </section>
-                                <section className="col col-img">
-                                    <img className="IphoneXMock" alt="profile" src={process.env.PUBLIC_URL + "/img/landing_mock.png"} />
-                                </section>
-                        </div>
-
-                    </main>
-
-                </header>
-            </div>
+            <hero>
+                <main role="main">
+                    <section className="hero jumbotron jumbotron-fluid bgdark">
+                        <section className="container">
+                            <section className="col col-text">
+                                <h1 className="display-4">
+                                    Better friendships,
+                                </h1>
+                                <h1 className="display-4">
+                                    stress-free.
+                                </h1>
+                                <p className="lead">
+                                    Superpotato is a personal assistant that helps you remember, manage, and grow your social network. 
+                                </p>
+                                <p className="lead">
+                                    Stop messing with spreadsheets and chillax like a real potato.
+                                </p>
+                                <button type="button" class="btn btn-primary">Get early access for free</button>
+                            </section>
+                            <section className="col col-img">
+                                <img className="IphoneXMock" alt="app mockup" src={process.env.PUBLIC_URL + "/img/landing_mock.png"} />
+                            </section>
+                        </section>
+                    </section>
+                </main>
+            </hero>
         )
     }
 
