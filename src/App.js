bf4221c5269e752b0b7e34808c079f15381dc024
sugoh <gsufen@gmail.com>
2020-01-31 00:11:54 +0800

    added process env PORT for heroku

--- src/App.js
+++ src/App.js
@@ -9,14 +9,11 @@ import Features from './components/Features.js';
 import Footer from './components/Footer.js';
 import FAQ from './components/FAQ.js';
 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 class App extends Component {
-
-  var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-  var server_host = process.env.YOUR_HOST || '0.0.0.0';
-  server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-  });
 
   render() {
     return (
