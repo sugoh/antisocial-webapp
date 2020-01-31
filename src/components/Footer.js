837d7604e9f32a412ff0ee66415739e8f16c5cb0
sugoh <gsufen@gmail.com>
2020-02-01 00:11:35 +0800

    Removed Contact Us link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,6 @@ class Footer extends Component {
                             <h3>The Good Stuff</h3>    
                             <ul>
                                 <li><a href={process.env.PUBLIC_URL + "/#faq"}>FAQ</a></li>
-                                <li>Contact us</li>
                                 <li><a href={process.env.PUBLIC_URL + "/files/Superpotato_terms_20200119.pdf"}>Terms</a></li>
                             </ul>
                         </section>
