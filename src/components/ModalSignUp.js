7f056c8b06f8d3e542da63974d09df257facbb12
sugoh <gsufen@gmail.com>
2020-02-02 17:38:11 +0800

    Copied Heroku branch to master tree

--- src/components/ModalSignUp.js
+++ src/components/ModalSignUp.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './Modal.css';
 import { Modal } from 'react-bootstrap';
 
-
 class ModalSignUp extends Component {
 
     constructor() {
@@ -22,20 +21,21 @@ class ModalSignUp extends Component {
 
     render() {
         return (
-            <div className="popUp">
+            <section className="popUp">
                 <button type="button" className="btn btn-primary" variant="success" onClick={() => { this.handleModal() }}>Get early access for free</button>
                 <Modal show={this.state.show} onHide={() => this.handleModal()}>
-                    <Modal.Header closeButton>
-                        <h3 className="modal-title">Ready to feel the magic?</h3>
-                        <h4 className="modal-subtitle">Sign up for early access today.</h4> 
-                    </Modal.Header>
+                    <Modal.Header closeButton></Modal.Header>
                     <Modal.Body>
+                        <section className="modal-header-wrapper">
+                            <h3 className="modal-title">Ready to feel the magic?</h3>
+                            <h4 className="modal-subtitle">Sign up for early access today.</h4> 
+                        </section>
                         <SignUp />
                     </Modal.Body>
                     <Modal.Footer>
                     </Modal.Footer>
                 </Modal>
-            </div>
+            </section>
         )
     }
 }
