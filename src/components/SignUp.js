5d890c71a96bc3784ccca524e83f59e446a53afa
cyhk <cyhk116@gmail.com>
2020-02-02 20:19:37 -0800

    switched SignUp form to controlled component

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,91 +1,131 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Modal.css'
-import { Form, Button, Col } from 'react-bootstrap/';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Modal.css";
+import { Form, Button, Col } from "react-bootstrap/";
 
 class SignUp extends Component {
-
-    constructor(props) {
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.state = {
-            validated: false,
-            setValidated: false,
-            status: ""
-        };
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      validated: false,
+      setValidated: false,
+      status: "",
+      formFields: {
+        first_name: "",
+        last_name: "",
+        email: ""
       }
+    };
 
-    render() {
-        const { status } = this.state;
-        return (
-            <Form className="SignUp" name="waitlist" validated={this.validated} onSubmit={this.handleSubmit} action="https://formspree.io/xbjodrgl" method="POST">
-                <Form.Group controlId="validationCustom01">
-                    <Col>
-                        <Form.Label className="showLabel">First Name</Form.Label>
-                        <input
-                            className="form-control"
-                            name="first_name"
-                            placeholder="First name"
-                            required
-                            type="text"
-                            placeholder=""
-                        />
-                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                    </Col>
-                </Form.Group>
-                <Form.Group controlId="validationCustom02">
-                    <Col>
-                        <Form.Label className="showLabel">Last Name</Form.Label>
-                        <input
-                            className="form-control"
-                            name="last_name"
-                            required
-                            type="text"
-                            placeholder=""
-                        />
-                    </Col>
-                </Form.Group>
-                <Form.Group controlId="formBasicEmail">
-                    <Col>
-                        <Form.Label className="showLabel" >Email address</Form.Label>
-                        <input
-                            className="form-control"
-                            name="email"
-                            required
-                            type="email"
-                            placeholder=""
-                        />
-                    </Col>
-                </Form.Group>
-                <Col className="d-flex justify-content-between w-100">
-                    <Button className="submitBtn" type="submit">Join waitlist</Button>
-                </Col>
-            </Form >
-        )
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleSubmit(event) {
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-            console.log("Failed");
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState(state => {
+      return {
+        ...state,
+        formFields: {
+          ...state.formFields,
+          [name]: value
         }
-        this.setState({ validated: true })
-        const form = event.target;
-        const data = new FormData(form);
-        const xhr = new XMLHttpRequest();
-        xhr.open(form.method, form.action);
-        xhr.setRequestHeader("Accept", "application.json");
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState !== XMLHttpRequest.DONE) return;
-            if (xhr.status === 200){
-                form.reset();
-                this.setState({ status: "SUCCESS" });
-            } else{
-                this.setState({ status: "ERROR" });
-            }
-        };
-        xhr.send(data);
+      };
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      console.log("Failed");
     }
+    this.setState({ validated: true });
+
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application.json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        this.setState({ status: "SUCCESS" });
+        this.setState({
+          formFields: {
+            first_name: "",
+            last_name: "",
+            email: ""
+          }
+        });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    const data = JSON.stringify(this.state.formFields);
+    xhr.send(data);
+  }
+
+  render() {
+    return (
+      <Form
+        className="SignUp"
+        name="waitlist"
+        validated={this.validated}
+        onSubmit={this.handleSubmit}
+        action="https://formspree.io/xbjodrgl"
+        method="POST"
+      >
+        <Form.Group controlId="validationCustom01">
+          <Col>
+            <Form.Label className="showLabel">First Name</Form.Label>
+            <input
+              className="form-control"
+              name="first_name"
+              placeholder="First name"
+              required
+              type="text"
+              value={this.state.formFields.firstName}
+              onChange={this.handleChange}
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Col>
+        </Form.Group>
+        <Form.Group controlId="validationCustom02">
+          <Col>
+            <Form.Label className="showLabel">Last Name</Form.Label>
+            <input
+              className="form-control"
+              name="last_name"
+              required
+              type="text"
+              placeholder=""
+              value={this.state.formFields.lastName}
+              onChange={this.handleChange}
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Col>
+            <Form.Label className="showLabel">Email address</Form.Label>
+            <input
+              className="form-control"
+              name="email"
+              required
+              type="email"
+              placeholder=""
+              value={this.state.formFields.email}
+              onChange={this.handleChange}
+            />
+          </Col>
+        </Form.Group>
+        <Col className="d-flex justify-content-between w-100">
+          <Button className="submitBtn" type="submit">Join waitlist</Button>
+        </Col>
+      </Form>
+    );
+  }
 }
 export default SignUp;
